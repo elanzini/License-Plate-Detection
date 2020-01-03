f79058383288ef74ec21b15c501987ed00004f76
edo <edoardo.lanzini@gmail.com>
2020-01-03 14:55:59 +0100

    write to CSV file

--- CaptureFrame_Process.py
+++ CaptureFrame_Process.py
@@ -24,32 +24,45 @@ THRESHOLD_SCENE = 0.9
 
 
 def CaptureFrame_Process(file_path, sample_frequency, save_path):
+
+    # Output csv
+    df = pd.DataFrame(columns=['License Plate', 'Frame no.', 'Timestamp(seconds)'])
+
+    # Time tracker + Frame Counter
     start_time = time.time()
-    last_frame = None
     frame_count = 0
+    scene_count = 0
+
+    last_frame = None
     cap = cv2.VideoCapture(file_path)
 
     while cap.isOpened():
 
         frame_count = frame_count + 1
-        print("frame number: " + str(frame_count))
 
         ret, frame = cap.read()
 
         if frame is None:
+            df.to_csv("outDetection.csv", encoding='utf-8', index=False)
             break
 
         if last_frame is None or Shot_Transition.get_histogram_correlation_grayscale(frame, last_frame) < THRESHOLD_SCENE:
 
-            cv2.imshow("NEW SCENE", frame)
+            # cv2.imshow("NEW SCENE", frame)
             plate_images = Localization.plate_detection(frame)
 
             for i in range(len(plate_images)):
                 # cv2.imshow("Plate " + str(i), plate_images[i])
+                # Compute Time and License Plate
                 end_time = time.time()
-                print("Found after: " + str((end_time - start_time)))
-                print("License Plate" + str(i) + ": " + Recognize.segment_and_recognize(plate_images[i]))
-            cv2.waitKey()
+                license_plate = Recognize.segment_and_recognize(plate_images[i])
+
+                df.loc[scene_count] = [license_plate] + [frame_count] + [end_time]
+                scene_count = scene_count + 1
+
+                # print("Found after: " + str((end_time - start_time)))
+                # print("License Plate" + str(i) + ": " + license_plate)
+            # cv2.waitKey()
 
             last_frame = frame
 
@@ -58,7 +71,7 @@ def CaptureFrame_Process(file_path, sample_frequency, save_path):
 
 
 class Output:
-    def __init__(self, frame_number, time_plate, plate_img=None, license_plate=None):
+    def __init__(self, frame_number, time_plate=0, plate_img=None, license_plate=None):
         self.plate_img = plate_img
         self.frame_number = frame_number
         self.time_plate = time_plate
