736b350e57b3693ba77d92d2e0dbbe12f2bc0997
Giorgio Acquati <g.acquati@student.tudelft.nl>
2020-01-13 01:09:26 +0100

    New light plate recognition

--- LightPlateRecognition.py
+++ LightPlateRecognition.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+import math
+import time
 
 aspect_ratio_threshold = 3
 
@@ -12,79 +14,87 @@ letter_component_height_min_ratio = 1 / 2
 
 expected_number_of_letters = 5
 
-kernel_2x2 = np.ones((2, 2), dtype="uint8")
-kernel_3x3 = np.ones((2, 2), dtype="uint8")
+kernel_2x2 = np.ones((2, 2), dtype='uint8')
+kernel_3x3 = np.ones((2, 2), dtype='uint8')
 
 letter_open_kernel = kernel_2x2
 
-max_expected_letters = 6
+expected_letters = 6
 
-DEBUG = False
+DEBUG = True
 debug_plate_letters_image_colors = np.array([
-    [255, 0, 0],
-    [0, 255, 0],
-    [0, 0, 255]
-], dtype="uint8")
+    [255, 255, 0],
+    [0, 255, 255],
+    [255, 0, 255]
+], dtype='uint8')
 
 histogram_bins_width = 5
 histogram_bins = np.arange(0, 255, histogram_bins_width)
 
+letter_threshold_ratio = 0.5
+
+min_cc_area_ratio = 0.15
+
+
+def get_mask_centroid_x(mask):
+
+    non_zero_points = np.where(mask > 0)[1]
+    centroid_x = round(np.mean(non_zero_points))
+
+    if math.isnan(centroid_x):
+        return int(round(mask.shape[1] / 2))
+
+    return int(centroid_x)
+
 
 def load_letter_templates():
     raw_templates = [
-        [cv2.imread("SameSizeLetters/1.bmp"), "B"],
-        [cv2.imread("SameSizeLetters/2.bmp"), "D"],
-        [cv2.imread("SameSizeLetters/3.bmp"), "F"],
-        [cv2.imread("SameSizeLetters/4.bmp"), "G"],
-        [cv2.imread("SameSizeLetters/5.bmp"), "H"],
-        [cv2.imread("SameSizeLetters/6.bmp"), "J"],
-        [cv2.imread("SameSizeLetters/7.bmp"), "K"],
-        [cv2.imread("SameSizeLetters/8.bmp"), "L"],
-        [cv2.imread("SameSizeLetters/9.bmp"), "M"],
-        [cv2.imread("SameSizeLetters/10.bmp"), "N"],
-        [cv2.imread("SameSizeLetters/11.bmp"), "P"],
-        [cv2.imread("SameSizeLetters/12.bmp"), "R"],
-        [cv2.imread("SameSizeLetters/13.bmp"), "S"],
-        [cv2.imread("SameSizeLetters/14.bmp"), "T"],
-        [cv2.imread("SameSizeLetters/15.bmp"), "V"],
-        [cv2.imread("SameSizeLetters/16.bmp"), "X"],
-        [cv2.imread("SameSizeLetters/17.bmp"), "Z"],
+        [cv2.imread('SameSizeLetters/1.bmp'), 'B'],
+        [cv2.imread('SameSizeLetters/2.bmp'), 'D'],
+        [cv2.imread('SameSizeLetters/3.bmp'), 'F'],
+        [cv2.imread('SameSizeLetters/4.bmp'), 'G'],
+        [cv2.imread('SameSizeLetters/5.bmp'), 'H'],
+        [cv2.imread('SameSizeLetters/6.bmp'), 'J'],
+        [cv2.imread('SameSizeLetters/7.bmp'), 'K'],
+        [cv2.imread('SameSizeLetters/8.bmp'), 'L'],
+        [cv2.imread('SameSizeLetters/9.bmp'), 'M'],
+        [cv2.imread('SameSizeLetters/10.bmp'), 'N'],
+        [cv2.imread('SameSizeLetters/11.bmp'), 'P'],
+        [cv2.imread('SameSizeLetters/12.bmp'), 'R'],
+        [cv2.imread('SameSizeLetters/13.bmp'), 'S'],
+        [cv2.imread('SameSizeLetters/14.bmp'), 'T'],
+        [cv2.imread('SameSizeLetters/15.bmp'), 'V'],
+        [cv2.imread('SameSizeLetters/16.bmp'), 'X'],
+        [cv2.imread('SameSizeLetters/17.bmp'), 'Z'],
 
-        [cv2.imread("SameSizeNumbers/0.bmp"), "0"],
-        [cv2.imread("SameSizeNumbers/1.bmp"), "1"],
-        [cv2.imread("SameSizeNumbers/2.bmp"), "2"],
-        [cv2.imread("SameSizeNumbers/3.bmp"), "3"],
-        [cv2.imread("SameSizeNumbers/4.bmp"), "4"],
-        [cv2.imread("SameSizeNumbers/5.bmp"), "5"],
-        [cv2.imread("SameSizeNumbers/6.bmp"), "6"],
-        [cv2.imread("SameSizeNumbers/7.bmp"), "7"],
-        [cv2.imread("SameSizeNumbers/8.bmp"), "8"],
-        [cv2.imread("SameSizeNumbers/9.bmp"), "9"],
+        [cv2.imread('SameSizeNumbers/0.bmp'), '0'],
+        [cv2.imread('SameSizeNumbers/1.bmp'), '1'],
+        [cv2.imread('SameSizeNumbers/2.bmp'), '2'],
+        [cv2.imread('SameSizeNumbers/3.bmp'), '3'],
+        [cv2.imread('SameSizeNumbers/4.bmp'), '4'],
+        [cv2.imread('SameSizeNumbers/5.bmp'), '5'],
+        [cv2.imread('SameSizeNumbers/6.bmp'), '6'],
+        [cv2.imread('SameSizeNumbers/7.bmp'), '7'],
+        [cv2.imread('SameSizeNumbers/8.bmp'), '8'],
+        [cv2.imread('SameSizeNumbers/9.bmp'), '9'],
     ]
 
     result = []
 
-    for letter_template_image_raw, letter in raw_templates:
-        raw_height = letter_template_image_raw.shape[0]
+    for letter_template_image, letter in raw_templates:
 
-        letter_template_image_raw = cv2.cvtColor(letter_template_image_raw, cv2.COLOR_BGR2GRAY)
+        letter_template_image = cv2.cvtColor(letter_template_image, cv2.COLOR_BGR2GRAY)
 
-        num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(letter_template_image_raw)
-
-        assert num_labels == 2
-
-        white_label = 1
-
-        letter_width = stats[white_label, cv2.CC_STAT_WIDTH]
+        height, width = letter_template_image.shape
 
         result.append({
-            raw_height: {
-                "image": letter_template_image_raw[:, :letter_width],
-                "center": (int(raw_height / 2), int(letter_width / 2)),
-                "size": (raw_height, letter_width)
+            height: {
+                'image': letter_template_image,
+                'centroid_x': get_mask_centroid_x(letter_template_image),
+                'width': width
             },
-            "letter": letter,
-            "default_height": raw_height
+            'letter': letter,
+            'default_height': height
 
         })
 
@@ -96,17 +106,19 @@ def resize_templates(templates, height):
 
         if height not in template:
             default_height = template['default_height']
-            default_template_image = template[default_height]["image"]
+            default_template_image = template[default_height]['image']
 
-            default_width = template[default_height]["size"][1]
+            default_width = template[default_height]['width']
 
             width = int(default_width * height / default_height)
 
             size = (width, height)
+            resized_template_image = cv2.resize(default_template_image, dsize=size, interpolation=cv2.INTER_NEAREST)
 
             template.update({
                 height: {
-                    "image": cv2.resize(default_template_image, dsize=size, interpolation=cv2.INTER_NEAREST),
+                    'image': resized_template_image,
+                    'centroid_x': get_mask_centroid_x(resized_template_image)
                 }
             })
 
@@ -114,62 +126,61 @@ def resize_templates(templates, height):
 letter_templates = load_letter_templates()
 
 
-def count_mismatches_same_size(image1, image2):
-    count = 0
+def count_mismatches(
+        image_0,
+        image_0_centroid,
+        image_1,
+        image_1_centroid,
+        height
+):
 
-    for i in range(image1.shape[0]):
-        for j in range(image1.shape[1]):
-            if image1[i, j] != image2[i, j]:
-                count += 1
+    # Left part
 
-    return count
+    if image_0_centroid > image_1_centroid:
 
+        new_image_1 = np.zeros((height, image_1.shape[1] + image_0_centroid - image_1_centroid), dtype="uint8")
+        new_image_1[:, image_0_centroid - image_1_centroid:] = image_1
+        image_1 = new_image_1
 
-def count_mismatches_different_size(min_width_image, max_width_image):
-    min_width = min_width_image.shape[1]
-    max_width = max_width_image.shape[1]
+    elif image_0_centroid < image_1_centroid:
 
-    height = min_width_image.shape[0]
+        new_image_0 = np.zeros((height, image_0.shape[1] + image_1_centroid - image_0_centroid), dtype="uint8")
+        new_image_0[:, image_1_centroid - image_0_centroid:] = image_0
+        image_0 = new_image_0
 
-    left_padding = int((max_width - min_width) / 2)
+    # Right part
 
-    right_padding = max_width - min_width - left_padding
+    image_0_width = image_0.shape[1]
+    image_1_width = image_1.shape[1]
 
-    mismatches = 0
+    if image_0_width > image_1_width:
 
-    # Left padding region
-    for y in range(height):
-        for x in range(left_padding):
-            if max_width_image[y, x] > 0:
-                mismatches += 1
+        new_image_1 = np.zeros((height, image_0_width), dtype="uint8")
+        new_image_1[:, :image_1_width] = image_1
+        image_1 = new_image_1
 
-    # Right padding region
+    elif image_1_width > image_0_width:
 
-    for y in range(height):
-        for x in range(left_padding + min_width, max_width):
-            if max_width_image[y, x] > 0:
-                mismatches += 1
+        new_image_0 = np.zeros((height, image_1_width), dtype="uint8")
+        new_image_0[:, :image_0_width] = image_0
+        image_0 = new_image_0
 
-    # Central part
-    for y in range(height):
-        for x in range(min_width):
-            if min_width_image[y, x] != max_width_image[y, x + right_padding]:
-                mismatches += 1
+    # Total
+    difference = cv2.absdiff(image_0, image_1)
 
-    return mismatches
+    return cv2.countNonZero(difference)
 
 
 def recognize_plate(plate, plate_color):
-    height, width, _ = plate.shape
-    plate_grayscale = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
+    plate = fix_perspective(plate)
 
     # Detecting edges
 
-    edges = cv2.Canny(plate, 100, 200)
+    edges = cv2.Canny(plate, 120, 200)
 
-    # Dilating edges
+    # Closing edges
 
-    edges = cv2.dilate(edges, kernel_2x2)
+    edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel_2x2)
 
     if DEBUG:
         cv2.imshow("edges", edges)
@@ -177,14 +188,20 @@ def recognize_plate(plate, plate_color):
     # Blob detection
     num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(edges, connectivity=8)
 
+    letters = []
+
+    if num_labels < expected_letters + 1:
+        return None
+
+    height, width, _ = plate.shape
+
+    min_component_height = int(letter_component_height_min_ratio * height)
+    max_component_width = width * letter_component_max_width_ratio
+
     centroid_lower_y_limit = int(height / 2 - centroid_y_threshold * height)
     centroid_upper_y_limit = int(height / 2 + centroid_y_threshold * height)
 
-    min_component_height = int(letter_component_height_min_ratio * height)
-
-    max_component_width = width * letter_component_max_width_ratio
-
-    letters = []
+    plate_grayscale = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
 
     for label in range(num_labels):
 
@@ -209,31 +226,27 @@ def recognize_plate(plate, plate_color):
 
         letter_grayscale = plate_grayscale[y:y + letter_height, x:x + letter_width]
 
-        letter_grayscale_histogram = np.histogram(letter_grayscale, bins=histogram_bins)[0]
+        letter_edges = edges[y:y + letter_height, x:x + letter_width]
+        threshold = np.mean(letter_grayscale[np.where(letter_edges > 0)])
 
-        avg = np.average(letter_grayscale)
-
-        last_black_bin_index = int(np.floor(avg / histogram_bins_width))
-
-        main_black_bin_index = np.argmax(letter_grayscale_histogram[:last_black_bin_index])
-        main_yellow_bin_index = np.argmax(letter_grayscale_histogram[last_black_bin_index + 1:]) + last_black_bin_index
-
-        threshold = int((main_yellow_bin_index + main_black_bin_index) / 2) * histogram_bins_width
-
-        letter_mask = np.zeros(letter_grayscale.shape, dtype="uint8")
+        letter_mask = np.zeros(letter_grayscale.shape, dtype='uint8')
         letter_mask[np.where(letter_grayscale < threshold)] = 255
 
         letter_mask = cv2.morphologyEx(letter_mask, cv2.MORPH_OPEN, letter_open_kernel)
 
         letters.append({
             'position': x,
-            'letter_mask': letter_mask
+            'letter_mask': letter_mask,
+            'centroid_x': get_mask_centroid_x(letter_mask)
         })
+
+    if len(letters) < expected_letters:
+        return None
 
     licence_plate_letters = []
 
     if DEBUG:
-        debug_plate_letters_image = np.zeros(plate.shape, dtype="uint8")
+        debug_plate_letters_image = np.zeros((plate.shape[0], plate.shape[1] + len(letters), 3), dtype='uint8')
         debug_plate_color_index = 0
         debug_plate_pointer = 0
 
@@ -241,7 +254,7 @@ def recognize_plate(plate, plate_color):
 
         letter_mask = letter['letter_mask']
 
-        recognized_letter, error_ratio = recognize_letter(letter_mask)
+        recognized_letter, error_ratio = recognize_letter(letter_mask, letter['centroid_x'])
 
         licence_plate_letters.append({
             'recognized_letter': recognized_letter,
@@ -251,25 +264,30 @@ def recognize_plate(plate, plate_color):
         })
 
         if DEBUG:
-            debug_colored_letter = np.zeros((letter_mask.shape[0], letter_mask.shape[1], 3), dtype="uint8")
+            debug_colored_letter = np.zeros((letter_mask.shape[0], letter_mask.shape[1], 3), dtype='uint8')
             debug_colored_letter[np.where(letter_mask > 0)] = debug_plate_letters_image_colors[debug_plate_color_index]
 
             debug_plate_color_index = (debug_plate_color_index + 1) % len(debug_plate_letters_image_colors)
 
             debug_plate_letters_image[
-            0:debug_colored_letter.shape[0],
-            debug_plate_pointer:debug_plate_pointer + letter_mask.shape[1]
+                :debug_colored_letter.shape[0],
+                debug_plate_pointer:debug_plate_pointer + letter_mask.shape[1]
             ] = debug_colored_letter
 
             debug_plate_pointer += letter_mask.shape[1]
 
-    if DEBUG:
-        cv2.imshow("debug plate", debug_plate_letters_image)
+            debug_plate_letters_image[:, debug_plate_pointer] = np.array([255, 255, 255], dtype='uint8')
 
-    if len(licence_plate_letters) > max_expected_letters:
+            debug_plate_pointer += 1
+
+    if DEBUG:
+
+        cv2.imshow('debug plate', debug_plate_letters_image)
+
+    if len(licence_plate_letters) > expected_letters:
 
         min_error_ratio_letters = sorted(licence_plate_letters, key=lambda letter: letter['error_ratio'])[
-                                  :max_expected_letters]
+                                  :expected_letters]
         min_error_ratio_letters = sorted(min_error_ratio_letters, key=lambda letter: letter['position'])
 
     else:
@@ -292,6 +310,7 @@ def recognize_plate(plate, plate_color):
 
             licence_plate.insert(1, '-')
             licence_plate.insert(5, '-')
+            print(str(gap_0_1) + "   " + str(gap_1_2))
 
         else:
             licence_plate.insert(2, '-')
@@ -309,7 +328,7 @@ def recognize_plate(plate, plate_color):
     return ''.join(licence_plate)
 
 
-def recognize_letter(letter_mask):
+def recognize_letter(letter_mask, letter_mask_centroid_x):
     letter_mask_height = letter_mask.shape[0]
 
     # Generating template matching size if not already existing
@@ -318,7 +337,7 @@ def recognize_letter(letter_mask):
     letter_mask_width = letter_mask.shape[1]
 
     min_mismatches = letter_mask.size
-    min_mismatches_letter = "!"
+    min_mismatches_letter = '!'
 
     error_ratio = 1
 
@@ -326,42 +345,41 @@ def recognize_letter(letter_mask):
 
         letter_template_matching_height = letter_template[letter_mask_height]
 
-        letter_template_image = letter_template_matching_height["image"]
+        letter_template_image = letter_template_matching_height['image']
 
         letter_template_width = letter_template_image.shape[1]
 
         if letter_mask_width > letter_template_width:
 
-            mismatches = count_mismatches_different_size(
-                min_width_image=letter_template_image,
-                max_width_image=letter_mask
-            )
             comparison_size = letter_mask.size
-
-        elif letter_mask_width < letter_template_width:
-
-            mismatches = count_mismatches_different_size(
-                min_width_image=letter_mask,
-                max_width_image=letter_template_image
-            )
-
-            comparison_size = letter_template_image.size
 
         else:
 
-            mismatches = count_mismatches_same_size(letter_template_image, letter_mask)
             comparison_size = letter_template_image.size
+
+        letter_template_centroid_x = letter_template_matching_height['centroid_x']
+
+        mismatches = count_mismatches(
+            image_0=letter_template_image,
+            image_0_centroid=letter_template_centroid_x,
+            image_1=letter_mask,
+            image_1_centroid=letter_mask_centroid_x,
+            height=letter_mask_height
+        )
 
         if mismatches < min_mismatches:
             min_mismatches = mismatches
-            min_mismatches_letter = letter_template["letter"]
+            min_mismatches_letter = letter_template['letter']
             error_ratio = min_mismatches / comparison_size
 
-    if min_mismatches_letter == "8":
+    """
+    if min_mismatches_letter == '8':
         min_mismatches_letter = verify_8_letter(letter_mask)
-
-    if min_mismatches_letter == "Z":
+    elif min_mismatches_letter == 'Z':
         min_mismatches_letter = verify_z_letter(letter_mask)
+    # elif min_mismatches_letter == '5' or min_mismatches_letter == 'S':
+    #     min_mismatches_letter = verify_5_S_letter(letter_mask)
+    """
 
     return min_mismatches_letter, error_ratio
 
@@ -382,9 +400,9 @@ def verify_8_letter(letter_mask):
     critical_region_size = critical_region_width * critical_region_height
 
     if cv2.countNonZero(critical_region) / critical_region_size > verify_8_critical_region_threshold:
-        return "B"
+        return 'B'
     else:
-        return "8"
+        return '8'
 
 
 verify_z_critical_region_threshold = 0.38
@@ -400,6 +418,51 @@ def verify_z_letter(letter_mask):
     critical_region_size = letter_mask.shape[0] * critical_region_height
 
     if cv2.countNonZero(critical_region) / critical_region_size > verify_z_critical_region_threshold:
-        return "Z"
+        return 'Z'
     else:
-        return "2"
+        return '2'
+
+
+expected_aspect_ratio = 300 / 66
+aspect_ratio_fix_threshold = 0.5
+
+
+def fix_perspective(licence_plate):
+    height, width, channels = licence_plate.shape
+
+    aspect_ratio = width / height
+
+    if np.abs(expected_aspect_ratio - aspect_ratio) > aspect_ratio_fix_threshold:
+
+        # Aspect ratio is really off, must be fixed
+
+        if aspect_ratio < expected_aspect_ratio:
+
+            new_size = (int(expected_aspect_ratio * height), height)
+        else:
+
+            new_size = (width, int(width / expected_aspect_ratio))
+
+        return cv2.resize(licence_plate, dsize=new_size)
+
+    return licence_plate
+
+
+verify_5_S_letter_threshold = 0.5
+
+
+def verify_5_S_letter(letter_mask):
+    critical_region_height = int(letter_mask.shape[0] / 2)
+    critical_region_width = int(letter_mask.shape[1] / 3.375)
+
+    critical_region_size = critical_region_height * critical_region_width
+
+    critical_region = letter_mask[:critical_region_height, :critical_region_width]
+
+    if cv2.countNonZero(critical_region) / critical_region_size > verify_5_S_letter_threshold:
+
+        return '5'
+
+    else:
+
+        return 'S'
---
+++ Test.py
@@ -0,0 +1,85 @@
+import cv2
+import LightLocalization
+import LightPlateRecognition
+import time
+
+filename = 'TrainingSet\Categorie I\Video2_2.avi'
+# filename = 'trainingsvideo.avi'
+
+cap = cv2.VideoCapture(filename)
+
+prev_plates_count = 0
+
+recognized_plates = {}
+
+plates_count = 0
+
+while cap.isOpened():
+
+    ret, frame = cap.read()
+
+    if frame is None:
+        break
+
+    cv2.imshow("frame", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    plates_color, plate_images = LightLocalization.locate_plates(frame, "yellow")
+
+    for i in range(len(plate_images)):
+        cv2.imshow("plate " + str(i), plate_images[i])
+
+    if len(plate_images) < prev_plates_count:
+        for i in range(len(plate_images), prev_plates_count + 1):
+            cv2.destroyWindow("plate " + str(i))
+
+    prev_plates_count = len(plate_images)
+
+    for i in range(len(plate_images)):
+
+        plate = LightPlateRecognition.recognize_plate(plate_images[i], plates_color)
+
+        if plate is not None:
+            plates_count += 1
+            print("Plate " + str(i) + ": " + plate)
+
+            if plate in recognized_plates:
+
+                recognized_plates.update({
+                    plate: recognized_plates[plate] + 1
+                })
+
+            else:
+
+                recognized_plates.update({
+                    plate: 1
+                })
+    time.sleep(1)
+
+most_common_plate = "!"
+most_common_plate_occurrences = 0
+
+for plate in recognized_plates:
+    if recognized_plates[plate] > most_common_plate_occurrences:
+        most_common_plate_occurrences = recognized_plates[plate]
+        most_common_plate = plate
+
+second_most_common_plate = '!'
+second_most_common_plate_occurrences = 0
+
+for plate in recognized_plates:
+    if recognized_plates[plate] > second_most_common_plate_occurrences and plate != most_common_plate:
+        second_most_common_plate_occurrences = recognized_plates[plate]
+        second_most_common_plate = plate
+
+print(str(most_common_plate_occurrences) + " -> " + most_common_plate)
+print(str(second_most_common_plate_occurrences) + " -> " + second_most_common_plate)
+if plates_count == 0:
+    print("Confidence: -1")
+else:
+    print("Confidence: " + str(most_common_plate_occurrences / plates_count))
+
+cap.release()
+cv2.destroyAllWindows()
